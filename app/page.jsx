0b8a5ec2363acbd45dc1f2e565bc9edf680b916a
حمدان راشد <hrh222333@gmail.com>
2025-10-18 18:59:08 +0300

    تعديل صفحة training assessment قصل السيرفر عن clinet

--- app/page.jsx
+++ app/page.jsx
@@ -154,7 +154,6 @@ export default function AuthPage() {
 
                 <div className="flex flex-col items-center mb-6">
                     {/* <Dumbbell className="w-16 h-16 text-emerald-500 dark:text-emerald-400 mb-3 animate-pulse" /> */}
-                    <img src="assets/icon2.png" alt="" className="w-50 h-50" />
                     <h1 className="text-3xl font-bold text-gray-800 dark:text-white text-center">
                         {t("register_title")}
                     </h1>
